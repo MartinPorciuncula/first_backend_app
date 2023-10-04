dfead3ac31a2b91bd78346e1e0615c2650a8dc3d
MartinPorciuncula <kgtprav@gmail.com>
2023-10-04 13:53:52 -0300

    Errors has been solved

--- src/app.js
+++ src/app.js
@@ -2,11 +2,6 @@ import express from "express";
 import { router } from './routes/routes.js'
 const app = express();
 
-app.listen(3100, () => {
-  console.log("Server is running on port 3100");
-});
-
-
 app.use(express.json());
 
  app.use("/api/v1", router)
