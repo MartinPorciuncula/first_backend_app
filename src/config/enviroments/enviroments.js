ddea498b85633280d86e7c746065af3ee59f20be
MartinPorciuncula <kgtprav@gmail.com>
2023-10-23 15:35:26 -0300

    finished the error handling codes

--- src/config/enviroments/enviroments.js
+++ src/config/enviroments/enviroments.js
@@ -5,5 +5,6 @@ export const envs = {
   PORT: env.get('PORT').required().asPortNumber(),
   DB_URI: env.get('DB_URI').required().asString(),
   SECRET_JWD_SEED: env.get("SECRET_JWD_SEED").required().asString(),
-  JWT_EXPIRE_IN: env.get("JWT_EXPIRE_IN").required().asString()
+  JWT_EXPIRE_IN: env.get("JWT_EXPIRE_IN").required().asString(),
+  NODE_ENV: env.get('NODE_ENV').required().asString(),
 }
