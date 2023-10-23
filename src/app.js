4150f90b3340d6594424926513dcfbaf807b32d1
MartinPorciuncula <kgtprav@gmail.com>
2023-10-23 15:47:25 -0300

    cors are added and used

--- src/app.js
+++ src/app.js
@@ -2,9 +2,12 @@ import express from "express";
 import { router } from './routes/routes.js'
 import { AppError } from "./errors/appError.js";
 import { globalErrorHandler } from "./errors/error.controller.js";
+import { enableCors } from "./plugins/cors.plugin.js";
 const app = express();
 
 app.use(express.json());
+const ACCEPTED_ORIGINS = ["http://localhost:3000","http://localhost:4000"]
+enableCors(app,ACCEPTED_ORIGINS)
 
  app.use("/api/v1", router)
 
